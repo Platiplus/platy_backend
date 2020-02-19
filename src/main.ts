72a98c756c14d0a5d67b214a210e5178f6c8e667
Victor Rosa <victor.rosa@dtidigital.com.br>
2020-02-19 18:06:51 -0300

    Heroku Build changes

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 
-const port = process.env.PORT || 3000;
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(port);
-  Logger.log(`Server is running on port ${port}`, 'Bootstrap');
+  app.enableCors();
+  await app.listen(process.env.PORT);
+  Logger.log(`Server is running on port ${process.env.PORT}`, 'Bootstrap');
 }
 bootstrap();
